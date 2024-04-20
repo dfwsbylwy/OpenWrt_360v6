17f54d30d3dd214e2eb950d27ae6d2deb8463e21
dfwsbylwy <45983357+dfwsbylwy@users.noreply.github.com>
2024-04-20 22:14:44 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.7.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
@@ -28,6 +28,10 @@ function git_sparse_clone() {
   mv -f $@ ../package
   cd .. && rm -rf $repodir
 }
+
+添加自定义lede luci app
+git_sparse_clone master https://github.com/coolsnowwolf/luci applications/luci-app-zerotier applications/luci-app-webadmin applications/luci-app-usb-printer applications/luci-app-cpufreq
+git clone --depth=1 https://github.com/sbwml/luci-app-qbittorrent package/luci-app-qbittorrent
 
 # 添加额外插件
 git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
